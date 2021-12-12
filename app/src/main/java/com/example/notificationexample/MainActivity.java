1fa908163deec6cdc7fe422ebd4fe52331c09dd0
Hasymi Al <68429761+zxyvyx@users.noreply.github.com>
2021-12-12 23:14:42 +0700

    Add: progress bar in notification

--- app/src/main/java/com/example/notificationexample/MainActivity.java
+++ app/src/main/java/com/example/notificationexample/MainActivity.java
@@ -28,44 +28,36 @@ public class MainActivity extends AppCompatActivity {
     public void displayNotification(View view) {
         createNotificationChannel();
 
-        Intent landingIntent = new Intent(this, LandingActivity.class);
-        landingIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        int max_progress = 100;
+        int current_progress = 0;
 
-        PendingIntent landingPendingIntent = PendingIntent.getActivity(this, 0, landingIntent, PendingIntent.FLAG_ONE_SHOT);
-
-        Intent yesIntent = new Intent(this, YesActivity.class);
-        yesIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        PendingIntent yesPendingIntent = PendingIntent.getActivity(this,0,yesIntent, PendingIntent.FLAG_ONE_SHOT);
-
-        Intent noIntent = new Intent(this, NoActivity.class);
-        noIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        PendingIntent noPendingIntent = PendingIntent.getActivity(this,0,noIntent, PendingIntent.FLAG_ONE_SHOT);
-
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_message)
-                .setContentTitle("Simple Notification")
-                .setContentText("This is a simple notification…")
+        final NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, CHANNEL_ID);
+        notificationBuilder.setSmallIcon(R.drawable.ic_download)
+                .setContentTitle("File Transfer")
+                .setContentText("Downloading…")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setAutoCancel(true).setContentIntent(landingPendingIntent)
-                .addAction(R.drawable.ic_message, "Yes", yesPendingIntent)
-                .addAction(R.drawable.ic_message, "No", noPendingIntent);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            RemoteInput remoteInput = new RemoteInput.Builder(TXT_REPLY).setLabel("Reply").build();
-
-            Intent replyIntent = new Intent(this, RemoteReceiver.class);
-            replyIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            PendingIntent replyPendingIntent = PendingIntent.getActivity(this, 0,
-                    replyIntent, PendingIntent.FLAG_ONE_SHOT);
-
-            NotificationCompat.Action action = new NotificationCompat.Action.Builder(R.drawable.ic_message,
-                    "Reply", replyPendingIntent).addRemoteInput(remoteInput).build();
-
-            notificationBuilder.addAction(action);
-        }
-
-        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+                .setProgress(max_progress, current_progress, false);
+        final NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
         notificationManagerCompat.notify(NOTIFICATION_ID, notificationBuilder.build());
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int count = 0;
+                    while (count <= 100) {
+                        count += 10;
+                        sleep(1000);
+                        notificationBuilder.setProgress(max_progress, count,false);
+                        notificationManagerCompat.notify(NOTIFICATION_ID, notificationBuilder.build());
+                    }
+                    notificationBuilder.setContentText("Download Complete")
+                            .setProgress(0, 0, false);
+                    notificationManagerCompat.notify(NOTIFICATION_ID, notificationBuilder.build());
+                } catch (InterruptedException error) { }
+            }
+        };
+        thread.start();
     }
 
     private void createNotificationChannel() {
