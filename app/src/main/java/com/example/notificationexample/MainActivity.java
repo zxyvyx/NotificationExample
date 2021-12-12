7ff70dc0ccc99e9fa3fd1775abda5b0f54b7022b
Hasymi Al <68429761+zxyvyx@users.noreply.github.com>
2021-12-12 23:35:02 +0700

    Add: image in expandable notification

--- app/src/main/java/com/example/notificationexample/MainActivity.java
+++ app/src/main/java/com/example/notificationexample/MainActivity.java
@@ -10,6 +10,8 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
@@ -27,42 +29,15 @@ public class MainActivity extends AppCompatActivity {
 
     public void displayNotification(View view) {
         createNotificationChannel();
-
-        Intent landingIntent = new Intent(this, LandingActivity.class);
-        landingIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
-
-        PendingIntent landingPendingIntent = PendingIntent.getActivity(this, 0, landingIntent, PendingIntent.FLAG_ONE_SHOT);
-
-        Intent yesIntent = new Intent(this, YesActivity.class);
-        yesIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        PendingIntent yesPendingIntent = PendingIntent.getActivity(this,0,yesIntent, PendingIntent.FLAG_ONE_SHOT);
-
-        Intent noIntent = new Intent(this, NoActivity.class);
-        noIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        PendingIntent noPendingIntent = PendingIntent.getActivity(this,0,noIntent, PendingIntent.FLAG_ONE_SHOT);
+        Bitmap coverBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.cover_image);
 
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_message)
                 .setContentTitle("Simple Notification")
                 .setContentText("This is a simple notification…")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setAutoCancel(true).setContentIntent(landingPendingIntent)
-                .addAction(R.drawable.ic_message, "Yes", yesPendingIntent)
-                .addAction(R.drawable.ic_message, "No", noPendingIntent);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            RemoteInput remoteInput = new RemoteInput.Builder(TXT_REPLY).setLabel("Reply").build();
-
-            Intent replyIntent = new Intent(this, RemoteReceiver.class);
-            replyIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            PendingIntent replyPendingIntent = PendingIntent.getActivity(this, 0,
-                    replyIntent, PendingIntent.FLAG_ONE_SHOT);
-
-            NotificationCompat.Action action = new NotificationCompat.Action.Builder(R.drawable.ic_message,
-                    "Reply", replyPendingIntent).addRemoteInput(remoteInput).build();
-
-            notificationBuilder.addAction(action);
-        }
+                .setLargeIcon(coverBitmap)
+                .setStyle(new NotificationCompat.BigPictureStyle().bigPicture(coverBitmap).bigLargeIcon(null));
 
         NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
         notificationManagerCompat.notify(NOTIFICATION_ID, notificationBuilder.build());
