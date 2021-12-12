c50bfc2dfd90b9ca5a4cae40762ca1a8b25c1d50
Hasymi Al <68429761+zxyvyx@users.noreply.github.com>
2021-12-12 21:13:56 +0700

    Add: notification for android 7 below

--- app/src/main/java/com/example/notificationexample/MainActivity.java
+++ app/src/main/java/com/example/notificationexample/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.notificationexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private final String CHANNEL_ID = "personal_notification";
+    private final int NOTIFICATION_ID = 001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +18,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void displayNotification(View view) {
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, CHANNEL_ID);
+        notificationBuilder.setSmallIcon(R.drawable.ic_message);
+        notificationBuilder.setContentTitle("Simple Notification");
+        notificationBuilder.setContentText("This is a simple notification…");
+        notificationBuilder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+        notificationManagerCompat.notify(NOTIFICATION_ID, notificationBuilder.build());
     }
 }
